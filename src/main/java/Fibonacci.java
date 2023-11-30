1952221d4521822fb0dd8bbd78fcbfb49db598b0
vanessabock <vanessabock54@gmail.com>
2023-11-30 14:34:30 +0100

    Added two fibonacci tests and fixed fibonacci method

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -2,12 +2,12 @@ public class Fibonacci {
 
     public static int fibonacci(int num) {
         int firstFibonacci = 0;
-        int secondFibonacci = 0;
+        int secondFibonacci = 1;
         int resultFibonacci = 0;
         for(int i = 0; i < num; i++){
-            secondFibonacci = firstFibonacci;
-            firstFibonacci = i;
             resultFibonacci = firstFibonacci + secondFibonacci;
+            firstFibonacci = secondFibonacci;
+            secondFibonacci = resultFibonacci;
         }
         return resultFibonacci;
     }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -4,22 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest{
     @Test
-    public void FibonacciTest_when1_then0(){
+    public void FibonacciTest_when1_then1(){
         //GIVEN
         int num = 1;
         //WHEN
         int actual = Fibonacci.fibonacci(num);
         //THEN
-        assertEquals(0,actual);
+        assertEquals(1,actual);
     }
     @Test
-    public void FibonacciTest_when2_then1(){
+    public void FibonacciTest_when2_then2(){
         //GIVEN
         int num = 2;
         //WHEN
         int actual = Fibonacci.fibonacci(num);
         //THEN
-        assertEquals(1,actual);
+        assertEquals(2,actual);
     }
     @Test
     public void FibonacciTest_when3_then3(){
@@ -29,5 +29,35 @@ class FibonacciTest{
         int actual = Fibonacci.fibonacci(num);
         //THEN
         assertEquals(3,actual);
+    }
+
+    @Test
+    public void FibonacciTest_when4_then5(){
+        //GIVEN
+        int num = 4;
+        //WHEN
+        int actual = Fibonacci.fibonacci(num);
+        //THEN
+        assertEquals(5,actual);
+    }
+
+    @Test
+    public void FibonacciTest_when5_then8(){
+        //GIVEN
+        int num = 5;
+        //WHEN
+        int actual = Fibonacci.fibonacci(num);
+        //THEN
+        assertEquals(8,actual);
+    }
+
+    @Test
+    public void FibonacciTest_when6_then13(){
+        //GIVEN
+        int num = 6;
+        //WHEN
+        int actual = Fibonacci.fibonacci(num);
+        //THEN
+        assertEquals(13,actual);
     }
 }
